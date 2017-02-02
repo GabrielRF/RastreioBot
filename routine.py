fe1bd646db5556bece34b3412d428ea997035dd1
GabrielRF <github@gabrf.com>
2017-02-02 14:43:28 -0200

    Infinite loop

--- routine.py
+++ routine.py
@@ -1,6 +1,6 @@
 from check_update import check_update
 from pymongo import MongoClient
-from time import time
+from time import time, sleep
 import sys
 
 client = MongoClient()
@@ -25,23 +25,28 @@ def get_package(code, user):
     return stat
 
 if __name__ == '__main__':
-    user = '9083329'
-    cursor1 = db.rastreiobot.find()
-    for elem in cursor1:
-        code = elem['code']
-        print(code)
-        old_state = elem['stat'][len(elem['stat'])-1]
-        # print('Old: ' + old_state)
-        cursor2 = db.rastreiobot.find_one(
-        {
-            "code": code
-        })
-        new_state = cursor2['stat'][len(elem['stat'])-1]
-        # print('New: ' + new_state)
-        users = elem['users']
-        if old_state != new_state:
-            print('Novidade')
-            for user in users:
-                print(user)
-        else:
-            print('Igual')
+    while True:
+        cursor1 = db.rastreiobot.find()
+        for elem in cursor1:
+            code = elem['code']
+            old_state = elem['stat'][len(elem['stat'])-1]
+            if 'Entrega Efetuada' in old_state:
+                continue
+            print(code)
+            # print('Old: ' + old_state)
+            cursor2 = db.rastreiobot.find_one(
+            {
+                "code": code
+            })
+            new_state = cursor2['stat'][len(elem['stat'])-1]
+            # print('New: ' + new_state)
+            users = elem['users']
+            if old_state != new_state:
+                print('Novidade')
+                for user in users:
+                    print(user)
+            else:
+                print('Igual')
+            sleep(3)
+        print('---')
+        sleep(60)
