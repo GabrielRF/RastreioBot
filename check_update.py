38fb9b7bfc7bcb10b0cbdd90d7525fb129233ba0
GabrielRF <github@gabrf.com>
2017-04-19 12:03:00 -0300

    Minor fixes

--- check_update.py
+++ check_update.py
@@ -9,10 +9,20 @@ def check_update(code):
         URL = ('http://websro.correios.com.br/sro_bin/txect01$.QueryList'
             +'?P_ITEMCODE=&P_LINGUA=001&P_TESTE=&P_TIPO=001&P_COD_UNI=')
         response = request.get(URL + code)
+        print('1: ' + str(response))
+#         if '200' not in str(response):
+#             print('Correios fora do ar')
+#             if '200' not in str(response):
+#                 URL = ('http://sro.micropost.com.br/consulta.php?objetos=')
+#                 response = request.get(URL + code)
+#                 print('2: ' + str(response))
     except:
-        print('Correios fora do ar')
         return 0
     if '200' not in str(response):
+        return 0
+    print(len(str(response)))
+    if len(str(response)) < 10:
+        print('aaaaaaa')
         return 0
     html = BeautifulSoup(response.content, 'html.parser')
     tabela = html.findAll('tr')
@@ -44,20 +54,20 @@ def check_update(code):
         else:
             observacao = False
         if data:
-            mensagem = 'Data: ' + data.text
+            mensagem = 'Data: ' + data.text.strip()
             if local:
-                mensagem = mensagem + '\nLocal: ' + local.text
+                mensagem = mensagem + '\nLocal: ' + local.text.strip()
             if situacao:
                 mensagem = (mensagem + '\nSituação: <b>' +
-                    situacao.text + '</b>')
+                    situacao.text.strip() + '</b>')
                 if situacao.text == 'Entrega Efetuada':
                     mensagem = mensagem + ' ' + str(u'\U0001F381')
                 elif situacao.text == 'Postado':
                     mensagem = mensagem + ' ' + str(u'\U0001F4E6')
-                elif situacao.text == 'Liberado sem':
-                    mensagem = mensagem + ' ' + str(u'\U0001F389')
             if observacao:
-                mensagem = mensagem + '\nObservação: ' + observacao.text
+                mensagem = mensagem + '\nObservação: ' + observacao.text.strip()
+                if 'Liberado sem' in observacao.text:
+                    mensagem = mensagem + ' ' + str(u'\U0001F389')
         stats.append(mensagem)
     # for elem in stats:
         # print(elem)
