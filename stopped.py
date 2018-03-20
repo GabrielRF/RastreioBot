390c0af7803fe6c9fcf3ae8b174e13977ddfb8f5
GabrielRF <github@gabrf.com>
2018-03-20 11:04:30 -0300

    Parado encaminha msgs

--- stopped.py
+++ stopped.py
@@ -24,7 +24,11 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.reply_to(message, "O bot está passando por uma rápida manutenção. Em breve tudo estará no ar novamente.")
+    bot.forward_message('9083329', message.from_user.id, message.message_id)
+    try:
+        bot.send_chat_action(message.chat.id, 'typing')
+        bot.reply_to(message, "O bot está passando por uma rápida manutenção. Em breve tudo estará no ar novamente.")
+    except:
+        pass
 
 bot.polling()
