215f41f034ab6c13b0dc3bcf37fa49c4d574afa3
Charliiee <carlin.afonso@gmail.com>
2019-09-24 15:41:05 -0300

    Busca produto em diversos carriers automaticamente conforme issue #36

--- apitrackingmore.py
+++ apitrackingmore.py
@@ -13,22 +13,28 @@ config.read('bot.conf')
 key = config['TRACKINGMORE']['key']
 trackingmore.set_api_key(key)
 
+carriers = ('cainiao', 'laos-post', 'dhl', 'sunyou')
+
 def get(code, times):
-    carrier = 'cainiao'
-    try:
-        td = trackingmore.create_tracking_data(carrier, code)
-        trackingmore.create_tracking_item(td)
-        td = trackingmore.get_tracking_item(carrier, code)
-    except trackingmore.trackingmore.TrackingMoreAPIException as e:
-        if e.err_code == 4019 or e.err_code == 4021:
-            return status.OFFLINE
-        if e.err_code == 4016: # Already exists
-            try:
-                td = trackingmore.get_tracking_item(carrier, code)
-            except trackingmore.trackingmore.TrackingMoreAPIException as e:
-                if e.err_code == 4019 or e.err_code == 4021:
-                    return status.OFFLINE
-    #print(td)
+    td = None
+    for carrier in carriers:
+        try:
+            td = trackingmore.create_tracking_data(carrier, code)
+            trackingmore.create_tracking_item(td)
+            td = trackingmore.get_tracking_item(carrier, code)
+        except trackingmore.trackingmore.TrackingMoreAPIException as e:
+            if e.err_code == 4019 or e.err_code == 4021:
+                return status.OFFLINE
+            if e.err_code == 4016: # Already exists
+                try:
+                    td = trackingmore.get_tracking_item(carrier, code)
+                except trackingmore.trackingmore.TrackingMoreAPIException as e:
+                    if e.err_code == 4019 or e.err_code == 4021:
+                        return status.OFFLINE
+        if td is not None and td['status'] != 'notfound':
+            break
+
+    print(td)
     if td['status'] == 'notfound':
         return status.NOT_FOUND_TM
     elif len(td) < 10:
@@ -38,7 +44,7 @@ def get(code, times):
 
 def formato_obj(json, carrier, code):
     stats = []
-    stats.append(str(u'\U0001F4EE') + ' <b>' + json['tracking_number'] + '</b>') 
+    stats.append(str(u'\U0001F4EE') + ' <b>' + json['tracking_number'] + '</b>')
     tabela = json['origin_info']['trackinfo']
     mensagem = ''
     for evento in reversed(tabela):
@@ -56,7 +62,7 @@ def formato_obj(json, carrier, code):
         ).format(data, situacao, observacao)
         stats.append(mensagem)
     return stats
-        
+
 
 if __name__ == '__main__':
     print(get(sys.argv[1], 0))
