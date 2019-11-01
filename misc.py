a394f1c58701ae0a472d65b38d71b3966db83ca8
Gabriel RF <github@gabrf.com>
2019-11-01 14:09:10 +0000

    One more tracking format allowed

--- misc.py
+++ misc.py
@@ -12,7 +12,7 @@ db = client.rastreiobot
 
 def check_type(code):
     s10 = (r"^[A-Za-z]{2}\d{9}[A-Za-z]{2}$")
-    ali = (r"^([A-Za-z]{2}\d{14}|[A-Za-z]{4}\d{9}|\d{10}|[A-Za-z]{5}\d{10}[A-Za-z]{2})$")
+    ali = (r"^([A-Za-z]{2}\d{14}|[A-Za-z]{2}\d{13}|[A-Za-z]{4}\d{9}|\d{10}|[A-Za-z]{5}\d{10}[A-Za-z]{2})$")
 
     if re.search(s10, str(code)):
         print('correios')
