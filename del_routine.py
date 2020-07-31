14a6b34a8b9e15f5c88720d86a6624c235a4f0bf
Marco Rougeth <marco@rougeth.com>
2020-07-31 18:42:53 +0100

    Simplifica e remove código duplicado do del_routine.py

--- del_routine.py
+++ del_routine.py
@@ -13,52 +13,46 @@ TOKEN = config['RASTREIOBOT']['TOKEN']
 int_del = int(config['RASTREIOBOT']['int_del'])
 LOG_DEL_FILE = config['RASTREIOBOT']['delete_log']
 
-logger_info = logging.getLogger('InfoLogger')
-logger_info.setLevel(logging.DEBUG)
-handler_info = logging.handlers.TimedRotatingFileHandler(LOG_DEL_FILE,
-    when='midnight', interval=1, backupCount=5, encoding='utf-8')
-logger_info.addHandler(handler_info)
+logger = logging.getLogger('InfoLogger')
+logger.setLevel(logging.DEBUG)
+handler = logging.handlers.TimedRotatingFileHandler(
+    LOG_DEL_FILE, when='midnight', interval=1, backupCount=5, encoding='utf-8'
+)
+logger.addHandler(handler)
 
 
 def del_user(code, msg):
-    logger_info.info(str(datetime.now()) + '\t' + code + '\t' + msg.replace('\n',' '))
+    logger.info("%s\t%s\t%r", datetime.now(), code, msg)
     db.delete_package(code)
 
 
 if __name__ == '__main__':
     cursor1 = db.all_packages()
-    logger_info.info(str(datetime.now()) + '\t' + '--- DELETE running! ---' )
+    logger.info('--- DELETE running! ---')
+
+    total_deleted = 0
+    final_status = [
+        'Entrega Efetuada',
+        'Objeto entregue ao destinatário',
+        'Objeto apreendido por órgão de fiscalização',
+        'Objetvo devolvido',
+        'Objetvo roubado',
+        'Aguardando recebimento pelo ECT.',
+        'Objeto não localizado no fluxo postal.',
+        'Delivered',
+    ]
+
     for elem in cursor1:
         code = elem['code']
-        time_dif = int(time() - float(elem['time']))
-        old_state = elem['stat'][len(elem['stat'])-1]
-        if 'Entrega Efetuada' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        elif 'Objeto entregue ao destinatário' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        elif 'Objeto apreendido por órgão de fiscalização' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        elif 'Objetvo devolvido' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        elif 'Objetvo roubado' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        elif 'Aguardando recebimento pelo ECT.' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        elif 'Aguardando recebimento pela ECT.' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        elif 'Objeto não localizado no fluxo postal.' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        elif 'Delivered' in old_state:
-            if time_dif > int_del:
-                del_user(elem['code'], old_state)
-        else:
-            if time_dif > 2 * int_del:
-                del_user(elem['code'], old_state)
+        time_diff = int(time() - float(elem['time']))
+        latest_status = elem['stat'][-1]
+
+        if (
+            any(status in latest_status for status in final_status)
+            and time_diff > int_del
+            or time_diff > 2 * int_del
+        ):
+            del_user(elem['code'], latest_status)
+            total_deleted += 1
+
+    logger.info("Total of packages deleted: %s", total_deleted)
