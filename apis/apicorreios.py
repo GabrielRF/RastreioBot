5900acbe0c8389956f03f29b1ee395d3cf36b99b
GabrielRF <github@gabrf.com>
2021-07-30 20:30:05 -0300

    Fixes semaphore and reduces timeouts

--- apis/apicorreios.py
+++ apis/apicorreios.py
@@ -17,8 +17,6 @@ usuario = config['CORREIOS']['usuario']
 senha = config['CORREIOS']['senha']
 token = config['CORREIOS']['token']
 
-semaphore = asyncio.Semaphore(40)
-
 def format_obj(code, response):
     stats = []
     result = json.loads(response)
@@ -139,7 +137,7 @@ def get(code, retries):
             #'http://webservice.correios.com.br/service/rastro/Rastro.wsdl'
         )
         response = requests.post(
-            url, data=request_xml, headers=headers, timeout=10
+            url, data=request_xml, headers=headers, timeout=3
         ).text
     except Exception:
         if retries > 0:
@@ -150,7 +148,11 @@ def get(code, retries):
         return status.OFFLINE
     elif 'ERRO' in str(response):
         return status.NOT_FOUND
-    return format_obj(code, response)
+    try:
+        return format_obj(code, response)
+    except json.decoder.JSONDecodeError as e:
+        return status.NOT_FOUND
+        #return 3
 
 async def async_get(code, retries):
     try:
@@ -175,23 +177,27 @@ async def async_get(code, retries):
             'http://webservice.correios.com.br/service/rest/rastro/rastroMobile'
         )
         async with aiohttp.ClientSession() as session:
-            async with semaphore, session.post(url, data=request_xml, headers=headers, timeout=30) as response:
+            async with session.post(url, data=request_xml, headers=headers, timeout=3) as response:
+                print(response.status, code)
+                status_code = response.status
                 response = await response.text()
     except Exception as e:
         if retries > 0:
             await asyncio.sleep(2)
             return await async_get(code, retries - 1)
         return status.OFFLINE
-    if len(str(response)) < 10:
+    if status_code != 200:
+        #return await async_get(code, retries - 1)
+        return status.OFFLINE
+    elif len(str(response)) < 10:
         return status.OFFLINE
     elif 'ERRO' in str(response):
         return status.NOT_FOUND
     try:
         return format_obj(code, response)
     except json.decoder.JSONDecodeError as e:
-        print("Error", e, response)
-        return 3
-
+        #print("Error", code, e, response)
+        return status.OFFLINE
 
 if __name__ == '__main__':
     print(get(sys.argv[1], 0))
