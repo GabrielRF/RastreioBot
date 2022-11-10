c9823b6ed344cb8442124de527c2ce23acd25ae0
Gabriel RF <github@gabrf.com>
2022-11-10 16:23:21 -0300

    Fixed description change

--- rastreiobot.py
+++ rastreiobot.py
@@ -651,6 +651,8 @@ def cmd_magic(message):
             if not db.package_has_user(code, user):
                 db.add_user_to_package(code, user)
             stats = db.package_status(code)
+            if desc != code:
+                db.set_package_description(code, user, desc)
             try:
                 desc = db.get_package_desc(code, user)
             except:
@@ -673,8 +675,6 @@ def cmd_magic(message):
                 )
             else:
                 send_clean_msg(bot, user, message)
-            if desc != code:
-                db.set_package_description(code, user, desc)
         else:
             stat = add_package(str(code), str(user))
             share_button = types.InlineKeyboardMarkup()
